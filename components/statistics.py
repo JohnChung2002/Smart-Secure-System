7a5f0851882d7ff178736ebfcf3e5113dffd9fac
John Chung <101233836@students.swinburne.edu.my>
2023-04-07 00:27:02 +0800

    Update statistics.py

--- components/statistics.py
+++ components/statistics.py
@@ -7,20 +7,18 @@ stats_bp = Blueprint('statistics', __name__)
 @auth_middleware
 def health_statistics():
     with g.dbconn:
-        results = g.dbconn.get_user_health_statistics(session["user_id"])
-        index = list(map(lambda x: x[1], results))
-        weight_value = list(map(lambda x: x[2], results))
-        height_value = list(map(lambda x: x[3], results))
-        bmi_value = list(map(lambda x: x[4], results))
-        return {"index": index, "weight_value": weight_value, "height_value": height_value, "bmi_value": bmi_value}, 200
+        return get_user_health_statistics(g.dbconn, session["user_id"])
     
 @stats_bp.route('/health_statistics/<id>')
 @admin_auth_middleware
 def health_statistics_by_id(id):
     with g.dbconn:
-        results = g.dbconn.get_user_health_statistics(id)
-        index = list(map(lambda x: x[1], results))
-        weight_value = list(map(lambda x: x[2], results))
-        height_value = list(map(lambda x: x[3], results))
-        bmi_value = list(map(lambda x: x[4], results))
-        return {"index": index, "weight_value": weight_value, "height_value": height_value, "bmi_value": bmi_value}, 200
+        return get_user_health_statistics(g.dbconn, id)
+    
+def get_user_health_statistics(conn, id):
+    results = conn.get_user_health_statistics(id)
+    index = list(map(lambda x: x[1].strftime('%Y-%m-%d'), results))
+    weight_value = list(map(lambda x: round(x[2], 2), results))
+    height_value = list(map(lambda x: round(x[3], 2), results))
+    bmi_value = list(map(lambda x: round(x[4], 2), results))
+    return {"index": index, "weight_value": weight_value, "height_value": height_value, "bmi_value": bmi_value}, 200
