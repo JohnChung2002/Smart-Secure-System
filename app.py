9f483209588756e5a30b2dd2ee44a42984e69f65
John Chung <101233836@students.swinburne.edu.my>
2023-04-06 18:12:12 +0800

    Update files

--- app.py
+++ app.py
@@ -10,16 +10,16 @@ sensor_config = {
 }
 
 app = Flask(__name__)
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-ser.reset_input_buffer()
+g.ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+g.ser.reset_input_buffer()
 app.secret_key = "E2DAD46AF8783EB848129379F1328"
 
 app.register_blueprint(auth)
 
 def read_serial_input():
     while True:
-        if (ser.in_waiting > 0):
-            temp = ser.readline()
+        if (g.ser.in_waiting > 0):
+            temp = g.ser.readline()
             print(temp)
             input = temp.decode('utf-8').rstrip().split("|")
             if (input[0] == "Entry" or input[0] == "Exit"):
@@ -38,10 +38,10 @@ def check_if_card_exists(card_id):
     with db:
         result = db.get_by_id("user_details", ["card_id"], [card_id])
         if (result is None):
-            ser.write(b"Invalid")
+            g.ser.write(b"Invalid")
         else:
             temp = f"Exists|{result[0]}|{result[1]}|{result[6]}"
-            ser.write(str.encode(temp))
+            g.ser.write(str.encode(temp))
 
 def insert_entry_exit(sensor_data):
     db = MySQLService('localhost', 'pi', 'pi', 'smart_lock_system')
@@ -73,19 +73,11 @@ def index():
     ]
     return render_template('dashboard.html', users=users)
 
-@app.route('/alarm-mode-on')
-def alarm_mode_on():
-    ser.write(b"Alarm On")
-    return "Alarm On", 200
 
-@app.route('/alarm-mode-off')
-def alarm_mode_off():
-    ser.write(b"Alarm Off")
-    return "Alarm Off", 200
 
 @app.route('/unlock')
 def unlock():
-    ser.write(b"Remote Unlock")
+    g.ser.write(b"Remote Unlock")
     return "Unlock", 200
 
 def request_has_connection():
---
+++ components/alarm.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, g
+from services.auth_middleware import auth_middleware
+from services.mysql_service import MySQLService
+
+alarm = Blueprint('alarm', __name__)
+
+@alarm.route('/alarm')
+@auth_middleware
+def alarm():
+    with g.dbconn:
+        result = g.dbconn.get_by_id("configs", ["config"], ["Alarm Status"])
+        status = "On" if result[1] == "Off" else "On"
+        status_message = f"Alarm {status}"
+        g.ser.write(str.encode(status_message))
+        g.dbconn.update("configs", ["value"], ["config"], [status, "Alarm Status"])
+    return status_message, 200
