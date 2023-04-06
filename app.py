18d97f26b9eeb0df57a79fb6bd70799eaa46d1ca
John Chung <101233836@students.swinburne.edu.my>
2023-04-06 12:03:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -100,4 +100,4 @@ sensor_thread.daemon = True
 sensor_thread.start()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
