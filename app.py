2fd980009f73e0ade12279bc815bff2c64b9e699
John Chung <101233836@students.swinburne.edu.my>
2023-04-06 18:21:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ sensor_config = {
 }
 
 app = Flask(__name__)
-g.ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-g.ser.reset_input_buffer()
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+ser.reset_input_buffer()
 app.secret_key = "E2DAD46AF8783EB848129379F1328"
 
 app.register_blueprint(auth_bp)
@@ -83,17 +83,24 @@ def unlock():
     return "Unlock", 200
 
 def request_has_connection():
-    return hasattr(g, 'dbconn')
+    return (
+        hasattr(g, 'dbconn') and g.dbconn is not None
+    ) and (
+        hasattr(g, 'ser') and g.ser is not None
+    )
 
 @app.before_request
 def get_request_connection():
     if not request_has_connection():
         g.dbconn = MySQLService('localhost', 'pi', 'pi', 'smart_lock_system')
+        g.ser = ser
 
 @app.teardown_request
 def close_db_connection(ex):
     if request_has_connection():
-        g.dbconn.close()
+        dbconn = g.pop('dbconn', None)
+        dbconn.close()
+        g.pop("ser", None)
 
 sensor_thread = Thread(target=read_serial_input)
 sensor_thread.daemon = True
