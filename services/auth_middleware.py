e0eea37fd0e57b51f76f07f2049192a0ba76b27d
John Chung <101233836@students.swinburne.edu.my>
2023-04-06 19:10:58 +0800

    Update auth_middleware.py

--- services/auth_middleware.py
+++ services/auth_middleware.py
@@ -1,10 +1,10 @@
 from flask import session, redirect, url_for
 
 def auth_middleware(func):
-    def wrapper(*args, **kwargs):
+    def wrapper_func(*args, **kwargs):
         if 'username' not in session:
             # user is not authenticated, redirect to login page
             return redirect(url_for('auth.login'))
         # user is authenticated, proceed to request handling
         return func(*args, **kwargs)
-    return wrapper
+    return wrapper_func
