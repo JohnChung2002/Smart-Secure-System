937df431a8804aa3c8ef3d7861c2101153d186ce
John Chung <101233836@students.swinburne.edu.my>
2023-04-06 19:23:08 +0800

    Update progress

--- Assignment_2.ino
+++ Assignment_2.ino
@@ -391,7 +391,6 @@ void loop() {
         startInOutScan();
       } else {
         if (checkIDInDatabase()) {
-          Serial.println("Hello");
           yellowLight();
           if (waitForRemoteApprove()) {
             startInOutScan();
