6e5d0e1ba36d824d00f2b9f768c2ec873abb7915
John Chung <101233836@students.swinburne.edu.my>
2023-04-13 19:07:45 +0800

    Update health_chart.js

--- static/js/health_chart.js
+++ static/js/health_chart.js
@@ -15,59 +15,58 @@ function getHealthStatistics() {
 
 function initialiseHealthCharts() {
     getHealthStatistics().then(function (data) {
-        // Create a chart
         var ctx = document.getElementById('healthChart').getContext('2d');
         var chart = new Chart(ctx, {
             type: 'line',
             data: {
-            labels: data.index,
-            datasets: [{
-                label: 'Height',
-                data: data.height_value,
-                borderColor: 'red',
-                fill: false
-            }, {
-                label: 'Weight',
-                data: data.weight_value,
-                borderColor: 'blue',
-                fill: false
-            }, {
-                label: 'BMI',
-                data: data.bmi_value,
-                borderColor: 'green',
-                fill: false
-            }]
-            },
-            options: {
-            responsive: true,
-            title: {
-                display: true,
-                text: 'Health Statistics'
-            },
-            tooltips: {
-                mode: 'index',
-                intersect: false
-            },
-            hover: {
-                mode: 'nearest',
-                intersect: true
-            },
-            scales: {
-                xAxes: [{
-                display: true,
-                scaleLabel: {
+                labels: data.index,
+                datasets: [{
+                    label: 'Height (cm)',
+                    data: data.height_value,
+                    borderColor: 'red',
+                    fill: false
+                    }, {
+                        label: 'Weight (kg)',
+                        data: data.weight_value,
+                        borderColor: 'blue',
+                        fill: false
+                    }, {
+                        label: 'BMI',
+                        data: data.bmi_value,
+                        borderColor: 'green',
+                        fill: false
+                    }
+                ]},
+                options: {
+                responsive: true,
+                title: {
                     display: true,
-                    labelString: 'Date'
+                    text: 'Health Statistics'
+                },
+                tooltips: {
+                    mode: 'index',
+                    intersect: false
+                },
+                hover: {
+                    mode: 'nearest',
+                    intersect: true
+                },
+                scales: {
+                    xAxis: [{
+                        display: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Date'
+                        }
+                    }],
+                    yAxis: [{
+                        display: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Value'
+                        }
+                    }]
                 }
-                }],
-                yAxes: [{
-                display: true,
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Value'
-                }
-                }]
-            }
             }
         });
     });
