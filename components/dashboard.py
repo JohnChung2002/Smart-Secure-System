67f80038a4c61960e614f270fbacfec26c7a87ff
John Chung <101233836@students.swinburne.edu.my>
2023-04-14 11:49:11 +0800

    Update dashboard.py

--- components/dashboard.py
+++ components/dashboard.py
@@ -11,6 +11,7 @@ def index():
         user = g.dbconn.get_by_id("user_details", ["user_id"], [session["user_id"]])
         alarm_status = g.dbconn.get_by_id("configs", ["config"], ["Alarm Status"])
         health_data = g.dbconn.get_user_average(session["user_id"])
+        num_people = g.dbconn.get_by_id("configs", ["config"], ["People in Room"])[1]
         if health_data[0] is not None and health_data[1] is not None and health_data[2] is not None:
             health_data = {
                 "weight": round(health_data[0], 2),
@@ -24,6 +25,7 @@ def index():
         'dashboard.html', 
         name=user[1], 
         role=session["user_role"], 
+        num_people=num_people,
         alarm_status=alarm_status[1],
         health_data=health_data,
         approval=approval
